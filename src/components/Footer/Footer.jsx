fb8b8be051bad9b6ba7c2841d771d5b9fb481585
n-rdy <giuseppegelardi@icloud.com>
2021-08-10 18:53:07 -0500

    adjusted footer implementation to sit at the bottom of the screen using a mix of bootstrap and css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,13 +3,7 @@ import './Footer.css'
 
 const Footer = (props) => {
   return ( 
-    <div className="row row-space">
-      <div className="col-md-12" style={{ padding: 5 }}>
-        <div className="footer">
-          <h4>footer</h4>
-        </div>
-      </div>
-    </div>
+    <footer class="footer text-center text-md">footer</footer>
    );
 }
  
