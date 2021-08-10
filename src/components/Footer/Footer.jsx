3f56bbba1329cc918c883d76c2389e1b0860cbe7
n-rdy <giuseppegelardi@icloud.com>
2021-08-10 18:37:52 -0500

    added footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = (props) => {
+  return ( 
+    <div className="row row-space">
+      <div className="col-md-12" style={{ padding: 5 }}>
+        <div className="footer">
+          <h4>footer</h4>
+        </div>
+      </div>
+    </div>
+   );
+}
+ 
+export default Footer;
