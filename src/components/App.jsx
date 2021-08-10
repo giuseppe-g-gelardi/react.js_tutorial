e1c78527bf700c55f4c5d555ac5877f386e2a021
n-rdy <giuseppegelardi@icloud.com>
2021-08-10 14:25:35 -0500

    deleted basically everything to start over from scratch

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return <h1>Our React App from A Component File</h1>
+  }
+}
+export default App
