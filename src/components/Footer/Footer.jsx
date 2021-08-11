e306bdff54ca97c60fc7bef0780b2e2e0d501d5e
n-rdy <giuseppegelardi@icloud.com>
2021-08-11 11:16:31 -0500

    completed tutorial project

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import './Footer.css'
+import './Footer.css';
 
-const Footer = (props) => {
-  return ( 
-    <footer class="footer text-center text-md">2021 - ggelardi INC</footer>
-    // please add more footer content
-   );
+// ! what i made
+// const Footer = (props) => {
+//   return ( 
+//     <footer class="footer text-center text-md">2021 - ggelardi INC</footer>
+     // TODO please add more footer content
+//    );
+// }
+
+// ? what dave made!
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h4>Copyright © 2021</h4>
+    </footer>
+  )
 }
- 
+
+
+
+
+
+
 export default Footer;
